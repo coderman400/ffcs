17ea183a362897fdd451620d54646b5c71b3ac77
arvind <achurox1121@gmail.com>
2024-09-09 00:28:54 +0530

    setup backend api calls

--- src/components/Submit.jsx
+++ src/components/Submit.jsx
@@ -1,22 +1,38 @@
 
 import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 function Submit(props) {
+  const [loading, setLoading] = useState(false);
   const show = props.show;
   const courseArray = props.courseArray;
   const navigate = useNavigate();
 
-  function handleSubmit() {
+ async function handleSubmit() {
     console.log("CLICKED RESULT");
-    navigate('/result', {state: {data: courseArray}});
+    setLoading(true)
+    try{
+      const response = await axios.post('http://192.168.59.109:8000/apis/combined/',{
+        courses: courseArray
+      });
+
+      const resultData = await response.data;
+
+      navigate('/result', {state: {data: resultData}})
+    }catch(e){
+      console.error('ERROR SENDING DATA: ', e)
+    }finally{
+      setLoading(false)
+    }
+    
   }
 
   return (
     <button
       onClick={handleSubmit}
-      style={{ display: show ? 'block' : 'none', justifySelf: 'center', marginLeft: '100%' }}
-    >
-      SUBMIT COURSES
+      style={{ display: show ? 'block' : 'none', justifySelf: 'center', marginLeft: '100%', disabled: {loading} } }>
+      {loading ? 'Loading... ': 'SUBMIT COURSES' }
     </button>
   );
 }

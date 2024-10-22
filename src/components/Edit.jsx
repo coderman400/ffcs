64d1be491d532840cff65ef7083451fbec3ea24c
arvind <achurox1121@gmail.com>
2024-10-22 06:17:36 +0530

    add mandatory fields to final data to send backend

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -2,14 +2,39 @@ import React from 'react'
 import './styles/edit.css'
 import { ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/16/solid'
 import { useState } from 'react'
+import axios from 'axios'
 const Edit = () => {
   const [expandedRows, setExpandedRows] = useState({});
+  const [mandatory , setMandatory] = useState({});
 
   const toggleExpansion = (index) => {
     setExpandedRows((prev) => ({
       ...prev,
       [index]: !prev[index]
     }))
+  }
+
+  const selectMandatory = (index) => {
+    setMandatory((prev) => ({
+      ...prev,
+      [index]: !prev[index]
+    }))
+  }
+  
+  const handleSubmit = async()=> {
+    let result = courses.map((course,index) => ({
+      code: course.code,
+      name: course.name,
+      slots: course.slots,
+      mandatory: mandatory[index] || false,
+    }))
+    
+    // try{
+    //   const response = await axios.post('API URL', result)
+    //   alert("data submitted!")
+    // }catch{
+    //   alert("error!")
+    // }
   }
 
   let courses = [
@@ -26,7 +51,7 @@ const Edit = () => {
   ]
 
   return (
-    <div className='flex items-center mt-20 justify-center'>
+    <div className='flex flex-col items-center mt-20 justify-center'>
       <table className='w-3/4 border shadow-md bg-white'>
         <thead className='bg-gray-800 text-white'> 
           <tr>
@@ -57,12 +82,16 @@ const Edit = () => {
                   </div>
                 ) }
               </td>
-              <td className='text-center'><input  className='w-20' type="checkbox"></input></td>
+              <td className='text-center'><input onClick={()=> selectMandatory(index)} className='w-20' type="checkbox"></input></td>
             </tr>
           ))
           }
         </tbody>
       </table>
+      <div className='w-3/4 flex justify-end'>
+        <button className='m-4 py-2 px-4  bg-white rounded-md shadow-md hover:bg-persian hover:text-white duration-150' onClick={()=>handleSubmit()}>Submit</button>
+      </div>
+      
     </div>
   )
 }
